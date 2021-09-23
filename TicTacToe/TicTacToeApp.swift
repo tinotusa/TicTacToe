f34e9d271b6ee758b48f54bfc83ce6ebd2c030af
Tino Tusaumwe <tusatino@gmail.com>
2021-09-23 13:45:45 +1000

    First commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Tino on 23/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
